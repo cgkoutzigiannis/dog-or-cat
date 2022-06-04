f15728e443b92b5303663a5469f76e9c11fa4313
PCined <chrisles143@gmail.com>
2022-06-04 06:21:47 +0300

    navbar change and about button platform

---
+++ django_project/static/js/show_about.js
@@ -0,0 +1,10 @@
+function aboutOnClick() {
+    const aboutWindow = document.getElementsByClassName("about-wrapper")[0];
+    aboutWindow.style.display = "flex";
+}
+
+function closeAbout() {
+    const aboutWindow = document.getElementsByClassName("about-wrapper")[0];
+    aboutWindow.style.display = "none";
+}
+
